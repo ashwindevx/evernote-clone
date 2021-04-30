6bf961061281afa0225da9317154a994c27c094b
ashwindevx <ashwinchauhan.developer@gmail.com>
2021-04-30 17:44:57 +0530

    Push Code

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase" ;
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA53PghrOmOjbbfmDEsOlgv3EV4q49f9AE",
+  authDomain: "evernote-clone-6ce1b.firebaseapp.com",
+  projectId: "evernote-clone-6ce1b",
+  storageBucket: "evernote-clone-6ce1b.appspot.com",
+  messagingSenderId: "713363442367",
+  appId: "1:713363442367:web:6cea21c3544322bc73d40a"
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want to start measuring performance in your app, pass a function
